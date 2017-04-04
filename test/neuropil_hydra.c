193f689f4feaef5624ad314bbfb4499bef97ce57
Stephan Schwichtenberg <stephan.schwichtenberg@pi-lar.net>
2017-04-04 23:21:49 +0200

    update hydra example to create a bootstrap node

--- test/neuropil_hydra.c
+++ test/neuropil_hydra.c
@@ -10,6 +10,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
@@ -67,58 +70,22 @@ int main(int argc, char **argv)
 			exit(1);
 		}
 	}
-	int current_pid = getpid();
-
-	int create_bootstrap = NULL == bootstrap_hostnode;
-	if (TRUE == create_bootstrap)
-	{
-		fprintf(stdout, "No bootstrap host specified.\n");
-		fprintf(stdout, "Creating new bootstrap node...\n");
-
-		char port[7];
-		if (current_pid > 65535) {
-			sprintf(port, "%d", (current_pid >> 1));
-		} else {
-			sprintf(port, "%d", current_pid);
-		}
-		char log_file_host[256];
-		sprintf(log_file_host, "%s_host.log", "./neuropil_hydra");
-		np_log_init(log_file_host, level);
-		np_init(proto, port, TRUE);
 
 
-		fprintf(stdout, "getting connection string\n");
-
-		bootstrap_hostnode = np_get_connection_string();
-
-		current_pid = fork();
-
-		if (0 == current_pid) {
-			np_start_job_queue(4);
-			np_waitforjoin();
-
-			while (TRUE) {
-				ev_sleep(3.1415 / 2);
-			}
-			// Outside of our thread
-			_exit(1);
-		}
-
-		if (TRUE == create_bootstrap) {
-			fprintf(stdout, "Bootstrap host node: %s\n", bootstrap_hostnode);
-			if (NULL == bootstrap_hostnode) {
-				fprintf(stderr,
-						"Bootstrap host node could not start ... exit\n");
-				exit(1);
-			}
-		}
-	}
+	int current_pid = 0;
 
 	np_sll_t(int, list_of_childs) = sll_init(int, list_of_childs);
 	int array_of_pids[required_nodes + 1];
 
-	current_pid = 0;
 	int status;
+
+	key_t key;
+	int shmid = 0;
+	key = ftok("./bin/neuropil_hydra", 'R');
+	shmid = shmget(key, 256, 0644);
+
+	// first clean up what was left from older runtime instances
+	shmctl(shmid, IPC_RMID, NULL);
 
 	while(TRUE)
 	{
@@ -129,31 +96,75 @@ int main(int argc, char **argv)
 
 			if (0 == current_pid)
 			{
-				fprintf(stdout, "started child process %d\n", current_pid);
-				current_pid = getpid();
+				// check for bootstrap node (creates shared memory)
+				key = ftok("./bin/neuropil_hydra", 'R');
+				shmid = shmget(key, 256, 0644);
 
-				char port[7];
-				if (current_pid > 65535)
+				if (shmid == -1)
 				{
-					sprintf(port, "%d", (current_pid >> 1));
+					fprintf(stdout, "No bootstrap host detected, creating a new one\n");
+
+					current_pid = getpid();
+					char port[7];
+					if (current_pid > 65535)
+					{
+						sprintf(port, "%d", (current_pid >> 1));
+					}
+					else
+					{
+						sprintf(port, "%d", current_pid);
+					}
+					char log_file_host[256];
+					sprintf(log_file_host, "%s_host.log", "./neuropil_hydra");
+
+					np_log_init(log_file_host, level);
+					np_init(proto, port, TRUE);
+
+					fprintf(stdout, "getting connection string\n");
+					bootstrap_hostnode = np_get_connection_string();
+
+					shmid = shmget(key, 256, 0644 | IPC_CREAT);
+					char* data = shmat(shmid, (void *)0, 0);
+
+					snprintf(data, 255, "%s", bootstrap_hostnode);
+					fprintf(stdout, "Bootstrap host node: %s\n", bootstrap_hostnode);
+
+					np_start_job_queue(4);
+					fflush(stdout);
 				}
 				else
 				{
-					sprintf(port, "%d", current_pid);
+					ev_sleep(3.1415);
+
+					char* data = shmat(shmid, (void *)0, SHM_RDONLY);
+
+					fprintf(stdout, "started child process %d\n", current_pid);
+					current_pid = getpid();
+
+					char port[7];
+					if (current_pid > 65535)
+					{
+						sprintf(port, "%d", (current_pid >> 1));
+					}
+					else
+					{
+						sprintf(port, "%d", current_pid);
+					}
+
+					char log_file[256];
+					sprintf(log_file, "%s_%s.log", "./neuropil_hydra", port);
+					// child process
+					np_log_init(log_file, level);
+					// used the pid as the port
+					np_init(proto, port, FALSE);
+
+					log_msg(LOG_DEBUG, "starting job queue");
+					np_start_job_queue(no_threads);
+					// send join message
+					log_msg(LOG_DEBUG, "creating welcome message");
+
+					np_send_join(data);
 				}
-
-				char log_file[256];
-				sprintf(log_file, "%s_%s.log", "./neuropil_hydra", port);
-				// child process
-				np_log_init(log_file, level);
-				// used the pid as the port
-				np_init(proto, port, FALSE);
-
-				log_msg(LOG_DEBUG, "starting job queue");
-				np_start_job_queue(no_threads);
-				// send join message
-				log_msg(LOG_DEBUG, "creating welcome message");
-				np_send_join(bootstrap_hostnode);
 
 				while (1)
 				{
@@ -161,7 +172,6 @@ int main(int argc, char **argv)
 				}
 				// escape from the parent loop
 				break;
-
 			}
 			else
 			{
@@ -171,6 +181,7 @@ int main(int argc, char **argv)
 				sll_append(int, list_of_childs, &array_of_pids[sll_size(list_of_childs)]);
 			}
 			ev_sleep(3.1415);
+
 		} else {
 
 			current_pid = waitpid(-1, &status, WNOHANG);
