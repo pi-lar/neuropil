e10889d70ffb70c5b98d9acfadf42c11f2acbbf1
Simon Klampt <simon.klampt@pi-lar.net>
2017-04-26 08:37:07 +0200

    Seperated Criterion Tests from others

--- test/test_suites.c
+++ test/test_suites.c
@@ -2,11 +2,16 @@
 // neuropil is copyright 2016 by pi-lar GmbH
 // Licensed under the Open Software License (OSL 3.0), please see LICENSE file for details
 //
-#include "ipv6_addrinfo.c"
-#include "jrb_test_msg.c"
+
+// Non Criterion tests:
+//#include "ipv6_addrinfo.c"
+//#include "jrb_test_msg.c"
+//#include "test_chunk_message.c"
+//#include "test_dh.c"
+//#include "test_sodium_crypt.c"
+
+// Criterion Tests:
 #include "test_aaatoken.c"
-#include "test_chunk_message.c"
-#include "test_dh.c"
 #include "test_jrb_impl.c"
 #include "test_jrb_serialization.c"
 #include "test_key.c"
@@ -16,6 +21,5 @@
 #include "test_message.c"
 #include "test_node.c"
 #include "test_route.c"
-#include "test_sodium_crypt.c"
 #include "test_suites.c"
 #include "test_util_uuid.c"
