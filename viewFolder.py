cd60c35b1d5dc1990cfefdb94a04b2bf98d47e52
Simon Klampt <simon.klampt@pi-lar.net>
2017-06-30 09:59:56 +0200

    only summarize .log and core files

--- viewFolder.py
+++ viewFolder.py
@@ -9,10 +9,12 @@ while True:
   print '-----------------------------------------'
   extensions = collections.defaultdict(int)
   subprocess.call("ls -al",shell=True)
-  for filename in os.listdir('.'):    
-      extensions[os.path.splitext(filename)[1].lower()] += 1
+  for filename in os.listdir('.'):
+      if(filename.endswith(".log")):
+        extensions[os.path.splitext(filename)[1].lower()] += 1
+      elif ("core" in filename):
+        extensions["core"] += 1
 
   for key,value in extensions.items():
     print 'Extension: ', key, '\t', value, '\titems'
   time.sleep(1)
-
