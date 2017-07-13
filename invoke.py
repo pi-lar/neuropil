3dc47ef9a4e2cdf8410540e0a520aac084f06cac
Simon Klampt <simon.klampt@pi-lar.net>
2017-07-13 15:49:47 +0200

    array shell

--- invoke.py
+++ invoke.py
@@ -4,4 +4,4 @@ import subprocess
 import os
 
 with open(os.devnull, 'w') as devnull:
-  subprocess.Popen("./bin/neuropil_raspberry","-g","1","-b","3333", env={"DYLD_LIBRARY_PATH":".;build/lib","LD_LIBRARY_PATH":".;build/lib"}, stdin=devnull, stdout=devnull, stderr=devnull)
+  subprocess.Popen(["./bin/neuropil_raspberry","-g","1","-b","3334"], env={"DYLD_LIBRARY_PATH":".;build/lib","LD_LIBRARY_PATH":".;build/lib"}, stdin=devnull, stdout=devnull, stderr=devnull)
