86241a381729118c001c4ff68454606009ae153d
Simon Klampt <simon.klampt@pi-lar.net>
2017-07-14 13:38:42 +0200

    startup script conf

--- invoke.py
+++ invoke.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+killall neuropil_raspberry
 rm *.log core
 export DYLD_LIBRARY_PATH=build/lib
 export LD_LIBRARY_PATH=build/lib
-./bin/neuropil_raspberry -g 1 -b 3334
+./bin/neuropil_raspberry -g 1 -b 3334 1&2> neuropil_raspberry.log < /dev/null &
+sleep 5s
+head -20 neuropil_raspberry.log
