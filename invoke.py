546efdef408f86bf9212ac831b4d3fa45255d44e
Simon Klampt <simon.klampt@pi-lar.net>
2017-07-20 12:28:35 +0200

    log

--- invoke.py
+++ invoke.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+echo "removing old instances"
 killall neuropil_raspberry
 rm *.log core
 export DYLD_LIBRARY_PATH=build/lib
 export LD_LIBRARY_PATH=build/lib
+echo "start program"
 ./bin/neuropil_raspberry -d -3 -g 1 -b 3334 >  neuropil_raspberry.log 2> neuropil_raspberry.log < neuropil_raspberry.log &
+echo "wait program"
 sleep 3s
 head -50 neuropil_raspberry.log
+echo "done"
