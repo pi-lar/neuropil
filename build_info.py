3073f3669a7d1d527e19b5d2209e8a09726e7c8c
Simon Klampt <devnull@localhost>
2017-09-14 12:23:59 +0200

    build info script

--- build_info.py
+++ build_info.py
@@ -2,19 +2,21 @@
 import os
 import re
 import platform as p
+import argparse
+import tarfile
 
 rx = re.compile("#define NEUROPIL_RELEASE	[\"'](.*)[\"']")
 
 def get_version():
     version = "could_not_detect_version"
-    dir_path = os.path.dirname(os.path.realpath(__file__))        
+    dir_path = os.path.dirname(os.path.realpath(__file__))
     with open("%s/include/neuropil.h"%(dir_path)) as f:
         for line in f:
             ver = rx.search(line)
             if(ver):
                 version = ver.group(1)
                 break
-            
+
     return version
 
 def get_version_tag():
@@ -22,7 +24,26 @@ def get_version_tag():
     return ("%s_alpha"% (get_version()))
 
 def get_build_name():
-    return  ("%s %s %s %s %s" % (get_version_tag(), p.system(), p.release(), p.machine(), p.architecture()[0]))
+    return  ("%s__%s__%s__%s" % (get_version_tag(), p.system(), p.release(), p.machine()))
 
 if __name__ == "__main__":
-    print get_build_name()
+    parser = argparse.ArgumentParser(description='Build helper.')
+    parser.add_argument('--build',help='build the tar file',action="store_true")
+    parser.add_argument('--version',help='prints the current version',action="store_true")
+    parser.add_argument('--versiontag',help='prints the current version tag',action="store_true")
+    args = parser.parse_args()
+
+    action = False
+    if args.build:
+        action = True
+        with tarfile.open("%s.tar.gz" % (get_build_name()), "w:gz") as tar:
+            tar.add("build/lib/", arcname=os.path.basename("build/lib/"))
+            tar.add("bin/", arcname=os.path.basename("bin/"))
+    if args.version:
+        action = True
+        print get_version()
+    if args.versiontag:
+        action = True
+        print get_version_tag()
+    if action != True:
+        parser.print_help()
