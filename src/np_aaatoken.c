fd3c511d305b1f6ad367a532b6da5c7268694977
Simon Klampt <devnull@localhost>
2017-10-16 08:43:24 +0200

    bf reffing in signature check

--- src/np_aaatoken.c
+++ src/np_aaatoken.c
@@ -355,10 +355,13 @@ np_bool _np_aaatoken_is_valid(np_aaatoken_t* token)
 		if (core_token_key != NULL){				
 
 			if (memcmp(core_token_key->aaa_token->signature, token->signature, crypto_sign_BYTES*(sizeof(unsigned char))) != 0) {
+
+				np_unref_obj(np_key_t, core_token_key, "_np_keycache_find");
 				log_msg(LOG_WARN, "Someone tried to impersonate a token. verification failed");
 				return (FALSE);
 			}
 		}
+		np_unref_obj(np_key_t, core_token_key, "_np_keycache_find");
 	}
 
 	log_debug_msg(LOG_AAATOKEN | LOG_DEBUG, "token checksum verification completed");	
