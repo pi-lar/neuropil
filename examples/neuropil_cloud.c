ebfa76aab4bf153e0746908d65682e94461c93fb
Simon Klampt <simon.klampt@pi-lar.net>
2018-05-25 15:58:39 +0200

    cloud test

---
+++ examples/neuropil_cloud.c
@@ -0,0 +1,86 @@
+//
+// neuropil is copyright 2016-2018 by pi-lar GmbH
+// Licensed under the Open Software License (OSL 3.0), please see LICENSE file for details
+//
+/**
+ *.. NOTE::
+ *
+ *   If you are not yet familiar with the neuropil initialization procedure please refer to the :ref:`tutorial`
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "np_interface.h" 
+#include "np_log.h"
+
+#define SIZE(ARRAY) (sizeof(ARRAY) / sizeof(ARRAY[0]))
+
+void make_wildcard(char* s) {
+	s[0] = '*';
+	
+	for (int i = 64; i <= strlen(s); i++) {
+		s[i - 63] = s[i];
+	}
+}
+
+int main(int argc, char **argv)
+{
+	np_context* nodes[200] = { 0 };
+
+	char addr[500];
+	uint16_t tmp;
+	for (int i=0; i < SIZE(nodes); i++) {
+		printf("INFO: Starting Node %d\n", i);
+		fflush(NULL);
+
+		int port = 3000 + i;
+		struct np_settings * settings = np_new_settings(NULL);		
+		sprintf(settings->log_file, "neuropil_cloud_%d.log", port);
+		settings->log_level |= LOG_MESSAGE;
+		settings->log_level |= LOG_ROUTING;
+		settings->log_level |= LOG_NETWORK;
+
+		nodes[i] = np_new_context(settings); // use default settings
+		
+		if (np_ok != (tmp = np_listen(nodes[i], "udp4", "localhost", port))) {
+			printf("ERROR: Node %d could not listen. %s\n", i, np_error_str[tmp]);
+		}
+		else {
+			if (np_ok != (tmp = np_get_address(nodes[i], addr, SIZE(addr)))) {
+				printf("ERROR: Could not get address of node %d. %s\n", i, np_error_str[tmp]);
+			}
+			printf("INFO: Node %d aka  (%s) listens\n", i, addr);			
+		}
+	}
+
+	uint16_t iteration = 0;
+	while (++iteration > 0)
+	{
+		for (int i = 0; i < SIZE(nodes); i++) {
+			if (np_ok != (tmp = np_run(nodes[i], 0.001))) {
+				printf("ERROR: Node %d could not run. %s\n", i, np_error_str[tmp]);
+			}
+			else {
+				if (i > 0 && iteration < SIZE(nodes)) {
+					// get connection str of previous node
+					if (np_ok != (tmp = np_get_address(nodes[i - 1], addr, SIZE(addr)))) {
+						printf("ERROR: Could not get address of node %d. %s\n", i, np_error_str[tmp]);
+					}
+					// for fun and testing make every second join a wildcard join			
+					//if (i % 2 == 0) 
+					{
+						make_wildcard(addr);
+					}
+					// join previous node			
+					if (np_ok != (tmp = np_join(nodes[i], addr))) {
+						printf("ERROR: Node %d could not join. %s\n", i, np_error_str[tmp]);
+					}
+					else {
+						printf("INFO: Node %d joins %s\n", i, addr);
+					}
+				}
+			}
+		}
+	}
+}
