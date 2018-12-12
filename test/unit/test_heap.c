2974ead0b9f0ec42eae36f541c520f3f0852d1db
stephan.schwichtenberg@pi-lar.net <stephan.schwichtenberg@pi-lar.net>
2018-12-12 08:15:36 +0100

    added np_job_t heap test case

--- test/unit/test_heap.c
+++ test/unit/test_heap.c
@@ -5,6 +5,8 @@
 #include <criterion/criterion.h>
 
 #include "np_heap.h"
+#include "np_jobqueue.h"
+#include "np_types.h"
 
 #include "../test_macros.c"
 
@@ -18,9 +20,9 @@ uint16_t int_binheap_get_priority(int ele) {
     return ele;
 }
 
-NP_BINHEAP_GENERATE_PROTOTYPES(int)
+NP_BINHEAP_GENERATE_PROTOTYPES(int);
 
-NP_BINHEAP_GENERATE_IMPLEMENTATION(int)
+NP_BINHEAP_GENERATE_IMPLEMENTATION(int);
 
 TestSuite(np_heap);
 
@@ -57,3 +59,67 @@ Test(np_heap, _np_heap_order, .description = "test the heap order")
 
     pheap_free(int, int_heap);
 }
+
+// already defined in np_jobqueue.h
+/*
+	bool np_job_t_compare(np_job_t new_ele, np_job_t j) {
+		return (new_ele.priority < j.priority);
+	}
+	uint16_t np_job_t_binheap_get_priority(np_job_t ele) {
+		return ele.priority;
+	}
+	NP_BINHEAP_GENERATE_PROTOTYPES(np_job_t);
+	NP_BINHEAP_GENERATE_IMPLEMENTATION(np_job_t);
+*/
+
+Test(np_heap, _np_heap_job_t, .description = "test the heap of a np_job_t")
+{
+    np_job_t tmp;
+
+    np_pheap_t(np_job_t, job_heap);
+    pheap_init(np_job_t, job_heap, 128);
+
+    cr_assert ( 0    == job_heap->count, "test that the current count of the job queue is zero");
+    cr_assert ( 128  == job_heap->size, "test that the maximum count of the job queue is 128");
+    cr_assert ( true == job_heap->elements[0].sentinel, "test that the spare element is tagged as sentinel");
+
+    np_job_t local_jobs[10];
+    for (uint8_t i = 0; i < 10; i++)  {
+    		local_jobs[i].priority = (double) i;
+    		local_jobs[i].exec_not_before_tstamp = 0.0;
+
+    		pheap_insert(np_job_t, job_heap, local_jobs[i]);
+    	    cr_assert ( i+1  == job_heap->count, "test that the current count of the job queue has increased");
+    	    cr_assert ( 128  == job_heap->size, "test that the maximum count of the job queue is still 128");
+    }
+    np_job_t test_element = { 0 };
+
+    test_element = pheap_first(np_job_t, job_heap);
+
+    cr_assert (  0.0  == test_element.priority, "test whether the first element has the lowest priority");
+    // modifying the returned test element should have no impact on the element in the heap
+    test_element.priority = 20.0;
+    cr_assert ( 20.0  == test_element.priority, "test whether the modified element has a changed priority");
+
+    test_element = pheap_head(np_job_t, job_heap);
+    cr_assert (  0.0   == test_element.priority, "test whether the first element still has the lowest priority");
+    cr_assert (  9     == job_heap->count, "test that the current count of the job queue has decreased");
+
+    test_element = pheap_first(np_job_t, job_heap);
+    cr_assert ( 0.0   != test_element.priority, "test whether the first element is a different element");
+    cr_assert ( 1.0   == test_element.priority, "test whether the first element has the lowest priority");
+
+    test_element = pheap_remove(np_job_t, job_heap, 29);
+    cr_assert ( 8     == job_heap->count, "test that the current count of the job queue has decreased");
+
+    test_element = pheap_remove(np_job_t, job_heap, 2);
+    cr_assert ( 7     == job_heap->count, "test that the current count of the job queue has decreased");
+    cr_assert ( 3.0   == test_element.priority, "test whether the first element has the lowest priority");
+
+    pheap_clear(np_job_t, job_heap);
+    cr_assert ( 0     == job_heap->count, "test that the current count of the job queue is zero");
+
+    pheap_free(np_job_t, job_heap);
+}
+
+
