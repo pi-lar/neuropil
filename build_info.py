497c50eb5993a75800b47830b7593338edd99c23
Simon Klampt <simon.klampt@pi-lar.net>
2019-01-21 12:57:54 +0100

    bf LICENCE and README in tar.gz release build sign key in dev

--- build_info.py
+++ build_info.py
@@ -14,17 +14,20 @@ rx = re.compile("#define NEUROPIL_RELEASE	[\"'](.*)[\"']")
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
+SIGN_KEY_FILE = "build_sign.key"
+
 def sign_file(filepath,pw):
     data = {
         'pw':pw,
         'filepath':filepath
     }
     cmds = [
-            ["openssl","dgst","-sha256","-sign","build_sign.key","-passin","pass:%(pw)s"% data,"-out","%(filepath)s.sig.raw"% data,"%(filepath)s"% data],
+            ["openssl","dgst","-sha256","-sign",SIGN_KEY_FILE,"-passin","pass:%(pw)s"% data,"-out","%(filepath)s.sig.raw"% data,"%(filepath)s"% data],
             ["openssl","base64","-in","%(filepath)s.sig.raw"% data, "-out", "%(filepath)s.sha256.sig" % data],
             ["rm","%(filepath)s.sig.raw"% data]
         ]
     for cmd in cmds:
+        #print("Calling: \""+" ".join(cmd)+"\"")
         subprocess.check_call(cmd)
 
 def sign_folder(folder,pw):    
@@ -36,9 +39,9 @@ def sign_folder(folder,pw):
 
 def get_version():
     version = "could_not_detect_version"
-    with open(os.path.join(dir_path,"include","neuropil.h")) as f:
+    with open(os.path.join(dir_path,"include","neuropil.h"),'rb') as f:
         for line in f:
-            ver = rx.search(line)
+            ver = rx.search(line.decode('UTF-8'))
             if(ver):
                 version = ver.group(1)
                 break
@@ -66,16 +69,23 @@ if __name__ == "__main__":
             print("missing parameter -pw in build")
             action = False
         else:
+            tarfilepath = "release/%s.tar.gz" % (get_build_name())
+            if not os.path.isfile(SIGN_KEY_FILE):    
+                print("Creating DEV sign key. DO NOT USE FOR TEST OR PRODUCTION!")            
+                subprocess.check_call(("openssl genpkey -algorithm RSA -out build_sign.key -pkeyopt rsa_keygen_bits:4096 -des3 -pass pass:"+args.pw).split(" "))
             action = True
             sign_folder("bin/",args.pw)
-            sign_folder("build/lib/",args.pw)
-            tarfilepath = "release/%s.tar.gz" % (get_build_name())
+            sign_folder("build/lib/",args.pw)            
             with tarfile.open(tarfilepath, "w:gz") as tar:
+                pass
                 tar.add("build/lib/", arcname=os.path.basename("build/lib/"))
                 tar.add("bin/", arcname=os.path.basename("bin/"))
-                tar.add("README", arcname=os.path.basename(""))
-                tar.add("LICENSE", arcname=os.path.basename(""))
-            sign_file(tarfilepath,args.pw)
+                tar.add("README", arcname=os.path.basename("README"))
+                tar.add("LICENSE", arcname=os.path.basename("LICENSE"))
+            print("Created TAR file in %s"%(tarfilepath))            
+            sign_file(tarfilepath, args.pw)
+            print("Signed  TAR file in %s"%(tarfilepath))            
+
     if args.version:
         action = True
         print(get_version())
