48176a544ef91bde70336c1796eb9f966a93c398
Simon Klampt <simon.klampt@pi-lar.net>
2020-01-31 16:54:17 +0100

    documenting the attributes api

--- include/neuropil_attributes.h
+++ include/neuropil_attributes.h
@@ -19,14 +19,14 @@ extern "C" {
         NP_ATTR_INTENT,
         NP_ATTR_IDENTITY,              // e.g. used when joining a network
         NP_ATTR_IDENTITY_AND_USER_MSG, // e.g. used to secure the bootstapping process. 
-        /**
-          Node "A" wants to connect to node "B"
-          "B" requests attr key "K1" to be given.  ("K1" is not in JOIN msg)
-          Node A registers by realm master "C"
-          "C" answers with a attribute "K1" configured for NP_ATTR_IDENTITY_AND_USER_MSG
-          "A" now saves "K1" in its identity token (so it can be saved) ("K1" is now in every JOIN msg)
-          "A" now preps every user message send with attribute "K1" (for example: SessionID)
-        **/
+        // Scenario for further documentation:        
+        // Node "A" wants to connect to node "B"
+        // "B" requests attr key "K1" to be given.  ("K1" is not in JOIN msg)
+        // Node A registers by realm master "C"
+        // "C" answers with a attribute "K1" configured for NP_ATTR_IDENTITY_AND_USER_MSG
+        // "A" now saves "K1" in its identity token (so it can be saved) ("K1" is now in every JOIN msg)
+        // "A" now preps every user message send with attribute "K1" (for example: SessionID)
+        
 
         NP_ATTR_INTENT_AND_USER_MSG,
         NP_ATTR_INTENT_AND_IDENTITY,
@@ -35,9 +35,9 @@ extern "C" {
 
 
     NP_API_EXPORT
-    enum np_return np_set_ident_attr_bin(struct np_token* ident, enum np_msg_attr_type  inheritance, char key[255], void* bin, size_t bin_length);
+    enum np_return np_set_ident_attr_bin(struct np_token* ident,       enum np_msg_attr_type  inheritance, char key[255], unsigned char * bin, size_t bin_length);
     NP_API_EXPORT
-    enum np_return np_set_mxp_attr_bin(struct np_mx_properties * prop, enum np_msg_attr_type  inheritance, void* bin, size_t bin_length);            
+    enum np_return np_set_mxp_attr_bin(struct np_mx_properties * prop, enum np_msg_attr_type  inheritance, char key[255], unsigned char * bin, size_t bin_length);            
     
     NP_API_EXPORT
     enum np_return np_get_msg_attr_bin(struct np_message * msg, char key[255], struct np_data_conf ** out_data_config, unsigned char ** out_data);
@@ -56,32 +56,43 @@ extern "C" {
 --------------
 Initialization
 --------------
-.. c:type:: void np_datablock_t
 
-   An opaque object that denotes a neuropil key/value store.
+.. c:type:: enum np_msg_attr_type
 
-.. c:function:: enum np_return np_init_datablock(np_datablock_t * block, size_t block_length)
-
-   Creates a new neuropil key value store.
-
-    :param block:        the memoryblock which should be initialized to contain a :c:type:`np_datablock_t`
-    :param block_length: a :c:type:`size_t` the size of the block input parameter
-    :return: :c:data:`np_ok` on success
-
-.. c:type:: struct np_data_conf
-
-   The configuration of a single key in an :c:type:`np_datablock_t`
-
-.. c:type:: enum np_data_type
-
-   This type denotes the set of data types relevant for :c:type:`np_datablock_t` key/value elements
+   This type denotes the set of inheritance levels relevant for a given attribute.
    Possible values include:
 
-   ================================================  ===========================================
-   Status                                            Meaning
-   ================================================  ===========================================
-   :c:data:`NP_DATA_TYPE_BIN`                        Define the key to hold binary data in its value container
-   :c:data:`NP_DATA_TYPE_INT`                        Define the key to hold an integer in its value container
-   :c:data:`NP_DATA_TYPE_STR`                        Define the key to hold a string in its value container (Uses ASCII encoding)
-   ================================================  ===========================================
+   ================================================  ======================================================================
+   Value                                             Meaning
+   ================================================  ======================================================================
+   :c:data:`NP_ATTR_USER_MSG`                        The attribute will be available in every USER message (end-to-end encrypted)   
+   :c:data:`NP_ATTR_IDENTITY`                        The attribute will be available in every JOIN message (end-to-end encrypted)
+   :c:data:`NP_ATTR_IDENTITY_AND_USER_MSG`           The attribute will be available in every JOIN and USER message (end-to-end encrypted)
+   :c:data:`NP_ATTR_INTENT`                          The attribute will be available only during the pub-sub discovery phase (transport encrypted)
+   :c:data:`NP_ATTR_INTENT_AND_USER_MSG`             
+   :c:data:`NP_ATTR_INTENT_AND_IDENTITY`             
+   ================================================  ======================================================================
+
+.. c:function:: enum np_return np_set_ident_attr_bin(struct np_token* ident, enum np_msg_attr_type  inheritance, char key[255], unsigned char * bin, size_t bin_length)
+
+   Sets an identity level wide attribute
+
+    :param ident:        the :c:type:`struct np_token` to set the attribute for
+    :param inheritance:  the :c:type:`enum np_msg_attr_type` inheritance level for this attribute
+    :param key:          the network wide identifier for this attribute
+    :param bin:          the attribute value as binary blob
+    :param bin_length:   the length of the blob
+    :return: :c:data:`np_ok` on success
+
+.. c:function:: enum np_return np_set_mxp_attr_bin(struct np_mx_properties* prop, enum np_msg_attr_type  inheritance, char key[255], unsigned char * bin, size_t bin_length)
+
+   Sets an message exchange level wide attribute
+
+    :param prop:         the :c:type:`struct np_mx_properties` to set the attribute for
+    :param inheritance:  the :c:type:`enum np_msg_attr_type` inheritance level for this attribute, only non NP_ATTR_IDENTITY... values are respected.
+    :param key:          the network wide identifier for this attribute
+    :param bin:          the attribute value as binary blob
+    :param bin_length:   the length of the blob
+    :return: :c:data:`np_ok` on success
+
 **/
