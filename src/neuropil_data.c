6e848e35f75d9e361f482c0d6b010baf609cb0e6
Simon Klampt <simon.klampt@pi-lar.net>
2020-05-05 11:59:02 +0200

    Data API: Simplyfy and cleanup

--- src/neuropil_data.c
+++ src/neuropil_data.c
@@ -16,7 +16,7 @@
 
 struct __np_datablock_s
 {
-    unsigned char * ublock;
+    unsigned char *ublock;
     cmp_ctx_t cmp;
     uint32_t total_length;
     uint32_t used_length;
@@ -37,30 +37,64 @@ struct __kv_pair
 
 enum np_return np_init_datablock(np_datablock_t *block, uint32_t block_length)
 {
-    if (block_length <= sizeof(uint32_t) * 3)
-    {
-        return np_invalid_argument;
-    }
-    memset(block, 0, block_length);
-    cmp_ctx_t cmp;
-    cmp_init(&cmp, block, NULL, NULL, _np_buffer_writer);
-    //_np_buffer_reader, _np_buffer_skipper
-
-    cmp_write_fixarray(&cmp, 4);
-    cmp_write_u32(&cmp, NP_DATA_MAGIC_NO);                     // magic_no
-    cmp_write_u32(&cmp, block_length);                         // total_length
-
+    enum np_return ret = np_ok;
     uint32_t overhead =
-        sizeof(uint8_t)      /*marker*/ + sizeof(uint8_t) /* value */ +             // fixarray
+        sizeof(uint8_t) /*marker+value*/ +                                          // fixarray
         3 * (sizeof(uint8_t) /*marker*/ + sizeof(uint32_t) /*value:->see right*/) + // magic_no + total_length + used_length
-        sizeof(uint8_t)      /*marker*/ + sizeof(uint8_t) /* value:object_count */  // map32
-    ;
-    cmp_write_u32(&cmp, overhead); // used_length
-    cmp_write_map32(&cmp, 0);
-    // prefilled with sizeof(magic_no + total_length + used_length)
+        sizeof(uint8_t) /*marker*/ + sizeof(uint32_t) /* value:object_count */      // map32
+        ;                                                                           // 21 byte
+    if (block_length <= overhead)
+    {
+        ret = np_invalid_argument;
+    }
+    else
+    {
+        memset(block, 0, block_length);
+        cmp_ctx_t cmp;
+        cmp_init(&cmp, block, NULL, NULL, _np_buffer_writer);
+        // fprintf(stderr, "overhead:%" PRIu32 "\n", overhead);
 
-    return np_ok;
+        if (!cmp_write_fixarray(&cmp, 4))
+        {
+            ret = np_unknown_error;
+        }
+        else if (!cmp_write_u32(&cmp, NP_DATA_MAGIC_NO)) // magic_no
+        {
+            ret = np_unknown_error;
+        }
+        else if (!cmp_write_u32(&cmp, block_length)) // total_length
+        {
+            ret = np_unknown_error;
+        }
+        else if (!cmp_write_u32(&cmp, overhead)) // used_length
+        {
+            ret = np_unknown_error;
+        }
+        else if (!cmp_write_map32(&cmp, (uint32_t) 0))  // object count
+        {
+            ret = np_unknown_error;
+        }
+    }
+    return ret;
 }
+enum np_return __read_datablock_fixed(enum np_return *error, struct __np_datablock_s * ret){
+    uint32_t magic_no, array_size;
+    if (!cmp_read_array(&ret->cmp, &array_size) && array_size == 4)
+    {
+        *error = np_invalid_argument;
+    }
+    else if (!cmp_read_u32(&ret->cmp, &magic_no) && NP_DATA_MAGIC_NO == magic_no) // magic_no
+    {
+        *error = np_invalid_argument;
+    }
+    else if (!cmp_read_u32(&ret->cmp, &ret->total_length)) // total_length
+    {
+        // fprintf(stderr, "__read_datablock.total_length\n");
+        *error = np_unknown_error;
+    }
+    return *error;
+}
+
 struct __np_datablock_s __read_datablock(np_datablock_t *block, enum np_return *error)
 {
     *error = np_ok;
@@ -69,27 +103,17 @@ struct __np_datablock_s __read_datablock(np_datablock_t *block, enum np_return *
     ret.ublock = ((unsigned char *)block);
     cmp_init(&ret.cmp, block, _np_buffer_reader, _np_buffer_skipper, _np_buffer_writer);
 
-    uint32_t magic_no = 0;
-    uint32_t array_size;
-    if(!cmp_read_array(&ret.cmp, &array_size) && array_size == 4){
-        *error = np_invalid_argument;
-    }
-    else if (!cmp_read_u32(&ret.cmp, &magic_no) && NP_DATA_MAGIC_NO == magic_no) // magic_no
-    {
-        *error = np_invalid_argument;
-    }
-    else if (!cmp_read_u32(&ret.cmp, &ret.total_length)) // total_length
-    {
-        fprintf(stderr, "__read_datablock.total_length");
-        *error = np_unknown_error;
+    if(__read_datablock_fixed(error, &ret) != np_ok) {
+        // fprintf(stderr, "__read_datablock_0\n");
     }
     else if (!cmp_read_u32(&ret.cmp, &ret.used_length)) // used_length
     {
-        fprintf(stderr, "__read_datablock.used_length");
+        // fprintf(stderr, "__read_datablock.used_length %" PRIu8 "\n", ret.cmp.error);
         *error = np_unknown_error;
-    }else if (!cmp_read_map(&ret.cmp, &ret.object_count)) // object_count
+    }
+    else if (!cmp_read_map(&ret.cmp, &ret.object_count)) // object_count
     {
-        fprintf(stderr, "__read_datablock.object_count");
+        // fprintf(stderr, "__read_datablock.object_count\n");
         *error = np_unknown_error;
     }
     return ret;
@@ -103,18 +127,19 @@ enum np_return __write_object(cmp_ctx_t *target, struct __kv_pair to_write)
 
     if (!cmp_write_str(target, to_write.key, key_len))
     {
-        fprintf(stderr, "__write_object.key");
+        // fprintf(stderr, "__write_object.key");
         ret = np_unknown_error;
     }
     else if (to_write.data_type == NP_DATA_TYPE_BIN)
     {
         if (!cmp_write_bin32(target, to_write.data.bin, to_write.data_size))
         {
-            fprintf(stderr, "__write_object.data_size");
+            // fprintf(stderr, "__write_object.data_size");
             ret = np_unknown_error;
         }
     } // ... other types
-    else{
+    else
+    {
         ret = np_invalid_argument;
     }
     return ret;
@@ -127,29 +152,26 @@ struct __kv_pair __read_object(cmp_ctx_t *cmp, enum np_return *error)
     uint32_t key_size = 255;
     if (!cmp_read_str(cmp, ret.key, &key_size)) // key
     {
-        fprintf(stderr, "__read_object.key");
+        // fprintf(stderr, "__read_object.key");
         *error = np_unknown_error;
     }
     else
     {
-        void *pre_type = cmp->buf;
         cmp_object_t type;
         if (!cmp_read_object(cmp, &type))
         {
             *error = np_unknown_error;
         }
+        else if (type.type == CMP_TYPE_BIN32)
+        {
+            ret.data_type = NP_DATA_TYPE_BIN;
+            ret.data_size = type.as.bin_size;
+            ret.data.bin = cmp->buf;
+            cmp->buf += ret.data_size;
+        } // ... other types
         else
         {
-            if (type.type == CMP_TYPE_BIN32)
-            {
-                ret.data_type = NP_DATA_TYPE_BIN;
-                ret.data_size = type.as.bin_size;
-                ret.data.bin = cmp->buf;
-                cmp->buf += ret.data_size;
-            } // ... other types
-            else{
-                *error = np_invalid_argument;
-            }
+            *error = np_invalid_argument;
         }
     }
     ret.end_of_object = cmp->buf;
@@ -206,7 +228,8 @@ enum np_return np_set_data(np_datablock_t *block, struct np_data_conf data_conf,
         {
             // check for space in block
             uint32_t new_object_size = sizeof(uint8_t) /*Marker*/ + strnlen(data_conf.key, 255) /*Key*/;
-            if(data_conf.type == NP_DATA_TYPE_BIN){
+            if (data_conf.type == NP_DATA_TYPE_BIN)
+            {
                 new_object_size += sizeof(uint8_t) /*Marker*/ + sizeof(uint32_t) /*DataSize*/ + data_conf.data_size /*Data*/;
             } // other data types
 
@@ -235,18 +258,23 @@ enum np_return np_set_data(np_datablock_t *block, struct np_data_conf data_conf,
                 // update "used_length"
                 if (ret == np_ok)
                 {
-                    unsigned char *overwrite = db.ublock + 2 /*marker */ + 2 * sizeof(uint32_t);
-                    cmp_ctx_t overwrite_cmp;
-                    cmp_init(&overwrite_cmp, overwrite, _np_buffer_reader, _np_buffer_skipper, _np_buffer_writer);
-
                     uint32_t new_used_length = (unsigned char *)db.cmp.buf - db.ublock;
+                    struct __np_datablock_s overwrite = {0};
+                    overwrite.ublock = ((unsigned char *)block);
+                    cmp_init(&overwrite.cmp, block, _np_buffer_reader, _np_buffer_skipper, _np_buffer_writer);
 
-                    if (!cmp_write_u32(&overwrite_cmp, new_used_length))
-                    {
-                        fprintf(stderr, "np_set_data.overwrite_used_length");
+                    if(__read_datablock_fixed(&ret,&overwrite) != np_ok){
+                        // fprintf(stderr, "np_set_data.__read_datablock_fixed\n");
                         ret = np_unknown_error;
-                    }else if(!cmp_write_map32(&overwrite_cmp, db.object_count + 1)){
-                        fprintf(stderr, "np_set_data.overwrite_object_count");
+
+                    }else if (!cmp_write_u32(&overwrite.cmp, new_used_length))
+                    {
+                        // fprintf(stderr, "np_set_data.overwrite_used_length\n");
+                        ret = np_unknown_error;
+                    }
+                    else if (!cmp_write_map32(&overwrite.cmp, db.object_count + 1))
+                    {
+                        // fprintf(stderr, "np_set_data.overwrite_object_count\n");
                         ret = np_unknown_error;
                     }
                 }
@@ -276,11 +304,13 @@ enum np_return np_get_data(np_datablock_t *block, char key[255], struct np_data_
     return ret;
 }
 
-    enum np_return np_get_data_size(np_datablock_t * block, size_t * out_block_size){
-        enum np_return ret = np_invalid_argument;
-        struct __np_datablock_s db = __read_datablock(block, &ret);
-        if(ret == np_ok){
-            *out_block_size = db.used_length;
-        }
-        return ret;
+enum np_return np_get_data_size(np_datablock_t *block, size_t *out_block_size)
+{
+    enum np_return ret = np_invalid_argument;
+    struct __np_datablock_s db = __read_datablock(block, &ret);
+    if (ret == np_ok)
+    {
+        *out_block_size = db.used_length;
     }
+    return ret;
+}
