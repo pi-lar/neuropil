c1051332a2c578a67258121aabf6fd0edb0393dd
TobiasWen <tobias@wentzlaff.net>
2020-08-17 17:28:28 +0200

    Extend sender/receiver example by user defined messages.

--- examples/neuropil_sender_threaded.c
+++ examples/neuropil_sender_threaded.c
@@ -9,6 +9,7 @@
 #include <stddef.h>
 #include <string.h>
 #include <assert.h>
+#include <pthread.h>
 #include <stdio.h>
 
 /**
@@ -24,7 +25,15 @@
    \endcode
 */
 
+struct input_thread_args {
+    np_context *ac;
+    bool isRunning;
+    const char *subject;
+};
+
+
 bool authorize (np_context *, struct np_token *);
+void handle_input(struct input_thread_args *args);
 
 int main (void)
 {
@@ -40,8 +49,11 @@ int main (void)
 	*/
 	struct np_settings cfg;
 	np_default_settings(&cfg);
-
 	np_context *ac = np_new_context(&cfg);
+
+    const char *subject = "mysubject";
+	struct np_mx_properties msg_props = np_get_mx_properties(ac, subject);
+	np_set_mx_properties(ac, subject, msg_props);
         /**
 	   \endcode
 	*/
@@ -88,39 +100,43 @@ int main (void)
 	   \endcode
 	*/
 
-    /**
-       Now to our application logic. We will repeatedly run the neuropil
-       event loop with :c:func:`np_run`, and then send our user defined
-       message with the subject ``"mysubject"`` using :c:func:`np_send`. If
-       anything goes wrong we return the error code (an
-       :c:type:`np_return`.)
+	/**
+	   Now to our application logic. We will repeatedly run the neuropil
+	   event loop for five seconds with :c:func:`np_run`, and then send our
+	   message with the subject ``"mysubject"`` using :c:func:`np_send` inside a dedicated thread. If
+	   anything goes wrong we return the error code (an
+	   :c:type:`np_return`.)
 
-       Effectively, this means that our node will process protocol requests
-       continuously (for as long as there is no error situation) and send a
-       message every five seconds periodically.
+	   Effectively, this means that our node will process protocol requests
+	   continuously and in parallel it is able to send a user defined message.
 
-       .. code-block:: c
+	   .. code-block:: c
 
-       \code
-     */
-    enum np_return status;
-    do {
-        status = np_run(ac, 0);
-        char message[100];
-        printf("Enter message (max 100 chars): ");
-        fgets(message, 200, stdin);
-        // Remove trailing newline
-        if ((strlen(message) > 0) && (message[strlen (message) - 1] == '\n'))
-            message[strlen (message) - 1] = '\0';
-        size_t message_len = strlen(message);
-        np_send(ac, "mysubject", message, message_len);
-        printf("Sent: %s\n", message);
-    } while (np_ok == status);
+	   \code
+	 */
+	enum np_return status;
 
+	// Create arguments for input thread
+	bool isRunning = true;
+	struct input_thread_args args;
+	args.ac = ac;
+	args.isRunning = isRunning;
+    args.subject = subject;
+    // Create & start user input thread
+	pthread_t input_thread;
+	pthread_create(&input_thread, NULL, handle_input, &args);
+
+	// Run neuropil event loop
+	do status = np_run(ac, 5.0);
+	while (np_ok == status);
+
+	// End thread loop and wait for it to end
+    isRunning = false;
+    pthread_join(input_thread, NULL);
 	return status;
         /**
 	   \endcode
-	*/
+	*/Extend sender/receiver example by user defined messages.
 }
 
 /**
@@ -142,3 +158,25 @@ bool authorize (np_context *ac, struct np_token *id)
 /**
    \endcode
 */
+
+/**
+ The user input will be dealt with in a separate thread. It will get the neuropil context and a running
+ indicator as arguments and will continously ask the user for new messages to send.
+ \code
+ */
+void handle_input(struct input_thread_args *thread_args) {
+	do {
+		char message[100];
+		printf("Enter message (max 100 chars): ");
+        fgets(message, 200, stdin);
+        // Remove trailing newline
+        if ((strlen(message) > 0) && (message[strlen (message) - 1] == '\n'))
+            message[strlen (message) - 1] = '\0';
+		size_t message_len = strlen(message);
+		np_send(thread_args->ac, thread_args->subject, message, message_len);
+		printf("Sent: %s\n", message);
+	} while(thread_args->isRunning);
+}
+/**
+ \endcode
+ */
