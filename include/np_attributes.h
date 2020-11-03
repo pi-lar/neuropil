61381685df6167640c78319a4695317f985077d0
Simon Klampt <simon.klampt@pi-lar.net>
2020-11-03 16:23:56 +0000

    Attribute function update

--- include/np_attributes.h
+++ include/np_attributes.h
@@ -3,53 +3,27 @@
 // Licensed under the Open Software License (OSL 3.0), please see LICENSE file for details
 //
 // original version is based on the chimera project
-#ifndef _NP_ATTR_H_
-#define _NP_ATTR_H_
+#ifndef _NP_ATTR_INNER_H_
+#define _NP_ATTR_INNER_H_
 
-#include "neuropil.h"
-#include "neuropil_data.h"
+#include "np_types.h"
+#include "neuropil_attributes.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-    enum np_msg_attr_type {
-        NP_ATTR_NONE = -1,
-        NP_ATTR_USER_MSG,
-        NP_ATTR_INTENT,
-        NP_ATTR_IDENTITY,              // e.g. used when joining a network
-        NP_ATTR_IDENTITY_AND_USER_MSG, // e.g. used to secure the bootstapping process.
-        // Scenario for further documentation:
-        // Node "A" wants to connect to node "B"
-        // "B" requests attr key "K1" to be given.  ("K1" is not in JOIN msg)
-        // Node A registers by realm master "C"
-        // "C" answers with a attribute "K1" configured for NP_ATTR_IDENTITY_AND_USER_MSG
-        // "A" now saves "K1" in its identity token (so it can be saved) ("K1" is now in every JOIN msg)
-        // "A" now preps every user message send with attribute "K1" (for example: SessionID)
-
-
-        NP_ATTR_INTENT_AND_USER_MSG,
-        NP_ATTR_INTENT_AND_IDENTITY,
-        NP_ATTR_MAX,
-
-    } NP_CONST_ENUM;
-
-    NP_API_EXPORT
-    enum np_data_return np_set_ident_attr_bin(np_context *ac, struct np_token* ident, enum np_msg_attr_type  inheritance, char key[255], unsigned char * bin, size_t bin_length);
-    NP_API_EXPORT
-    enum np_data_return np_set_mxp_attr_bin(np_context *ac,   char * subject,         enum np_msg_attr_type  inheritance, char key[255], unsigned char * bin, size_t bin_length);
-
-    NP_API_EXPORT
-    enum np_data_return np_get_msg_attr_bin(struct np_message * msg, char key[255], struct np_data_conf ** out_data_config, unsigned char ** out_data);
-    //NP_API_EXPORT
-    //enum np_data_return np_get_attr_bin(np_mx_properties * prop, char key[255], struct np_data_conf ** out_data_config, unsigned char ** out_data);
-    NP_API_EXPORT
-    enum np_data_return np_get_token_attr_bin(struct np_token* ident, char key[255], struct np_data_conf ** out_data_config, unsigned char ** out_data);
+    // neuropil setup functions
+    NP_API_PROTEC
+    bool _np_attributes_init(np_state_t* context);
+    NP_API_PROTEC
+    void _np_attributes_destroy(np_state_t* context);
+    NP_API_PROTEC
+    np_attributes_t* _np_get_attributes_cache(np_state_t* context, enum np_msg_attr_type cache);
 
 #ifdef __cplusplus
 }
 #endif
-#endif /* _NP_ATTR_H_ */
+#endif /* _NP_ATTR_INNER_H_ */
 
 /**
 --------------
