91d7bda4b3186c42525994d27233040b3d2ef95b
stephan schwichtenberg <stephan.schwichtenberg@pi-lar.net>
2020-11-18 15:11:09 +0100

    cherry picking simple cache improvements

--- src/np_scache.c
+++ src/np_scache.c
@@ -4,125 +4,142 @@
 //
 // Based upon http://stackoverflow.com/a/1234738
 
-#include <string.h>
-#include <stdlib.h>
-#include "np_scache.h"
-#include "np_list.h"
-
-#include "np_scache.h"
-
-#include "np_threads.h"
-#include "np_log.h"
-#include "np_legacy.h"
+#include "stdlib.h"
 #include "inttypes.h"
 
-NP_SLL_GENERATE_IMPLEMENTATION_COMPARATOR(np_cache_item_ptr);
-NP_SLL_GENERATE_IMPLEMENTATION(np_cache_item_ptr);
+#include "np_scache.h"
 
-np_simple_cache_table_t* np_cache_init(np_state_t* context) {
+#include "np_legacy.h"
+#include "np_list.h"
+#include "np_log.h"
+#include "np_threads.h"
+#include "np_util.h"
 
-	np_simple_cache_table_t* ret = 
-		(np_simple_cache_table_t*)malloc(
-		sizeof(np_simple_cache_table_t));
-	CHECK_MALLOC(ret);
-
-    char mutex_str[64];
-    snprintf(mutex_str, 63, "%s:%p", "urn:np:cache:access", ret);
-	_np_threads_mutex_init(context, &ret->lock, mutex_str);
-
-	for (uint32_t i = 0; i < SIMPLE_CACHE_NR_BUCKETS; i++) { 
-		sll_init(np_cache_item_ptr, ret->buckets[i]);
-	}
-
-	return ret; 
-}
-void np_cache_destroy(np_state_t* context, np_simple_cache_table_t* cache){
-
-	for (uint32_t i = 0; i < SIMPLE_CACHE_NR_BUCKETS; i++) { 
-		sll_iterator(np_cache_item_ptr) iter_bucket_item = sll_first(cache->buckets[i]);
-		while(iter_bucket_item != NULL) {
-			if(iter_bucket_item->val != NULL ) {
-				if(iter_bucket_item->val->key != NULL) 
-				{
-					free(iter_bucket_item->val->key);				
-				}
-				free(iter_bucket_item->val);
-			}
-			sll_next(iter_bucket_item);
-		}
-
-
-		sll_free(np_cache_item_ptr, cache->buckets[i]);
-	}
-	_np_threads_mutex_destroy(context, &cache->lock);
-
-	free(cache);
-}
-np_cache_item_t* np_simple_cache_get(np_state_t* context, np_simple_cache_table_t *table, const char* const key)
+// start of sll list code generation
+int8_t 
+np_cache_item_t_sll_compare_type(np_cache_item_t const a, np_cache_item_t const b) 
 {
-	log_trace_msg(LOG_TRACE, "start: np_cache_item_t* np_simple_cache_get(np_simple_cache_table_t *table, const char *key){");
-	
-	assert(NULL != key);
-
-	np_cache_item_t* ret = NULL;
-	_LOCK_ACCESS(&table->lock) {
-
-		uint32_t bucket = _np_simple_cache_strhash(key) % SIMPLE_CACHE_NR_BUCKETS;
-
-		np_sll_t(np_cache_item_ptr, bucket_list) = table->buckets[bucket];
-		sll_iterator(np_cache_item_ptr) iter = sll_first(bucket_list);
-		do
-		{
-			if(NULL != iter && NULL != iter->val && strcmp(iter->val->key, key) == 0){
-				ret =  iter->val;
-				break;
-			}
-		} while (NULL != ( sll_next(iter)) );
-	}
-	return ret;
+    return strncmp(a.key, b.key, strnlen(a.key, 255)); 
 }
 
-int np_simple_cache_insert(np_state_t* context, np_simple_cache_table_t *table, const char* const key, void *value) {
-	log_trace_msg(LOG_TRACE, "start: int np_simple_cache_insert(context, np_simple_cache_table_t *table, char *key, void *value) {");
-	// Contract
-	if(NULL == key){
-		log_msg(LOG_ERROR, "cache key cannot be NULL!");
-		abort();
-	}
-	// Contract end
+NP_SLL_GENERATE_IMPLEMENTATION(np_cache_item_t);
+// end of sll list code generation
 
-	_LOCK_ACCESS(&table->lock){
-		uint32_t bucket = _np_simple_cache_strhash(key) % SIMPLE_CACHE_NR_BUCKETS;
+void 
+np_cache_init(np_state_t* context, np_simple_cache_table_t* table, const uint16_t size, const unsigned char* seed) 
+{
+    table->_bucket_size = size;
+    memcpy(table->_seed, seed, crypto_shorthash_KEYBYTES);
 
-		np_sll_t(np_cache_item_ptr, bucket_list) = table->buckets[bucket];
-		sll_iterator(np_cache_item_ptr) iter = sll_first(bucket_list);
-		do
-		{
-			if(NULL != iter && NULL != iter->val && strncmp(iter->val->key, key, strlen(iter->val->key) ) == 0){
-				break;
-			}
-		} while (NULL != (sll_next(iter)) );
+    table->_bucket = calloc(table->_bucket_size, sizeof(np_cache_item_t_sll_t));
+    table->_bucket_guard = calloc(table->_bucket_size, sizeof(np_spinlock_t));
 
-		np_cache_item_t* item;
-
-		if(NULL == iter) {
-			item = (np_cache_item_t*) malloc(sizeof (np_cache_item_t));
-			CHECK_MALLOC(item);			
-			item->key = strndup(key, strlen(key));
-			sll_append(np_cache_item_ptr, bucket_list, item);
-		}else{
-			item = iter->val;
-		}
-		item->value = value;
-		item->insert_time = np_time_now();
-	}
-	return 0;
+    for (uint16_t i = 0; i < table->_bucket_size; i++) 
+    {
+        np_spinlock_init( (&table->_bucket_guard[i]), PTHREAD_PROCESS_PRIVATE);
+    }
 }
 
-uint32_t _np_simple_cache_strhash(const char* const str) {
-	uint32_t hash = 0;
-	const char* str_ptr = str;
-	for (; *str_ptr; str_ptr++)
-		hash = 31 * hash + *str_ptr;
-	return hash;
+void 
+np_cache_destroy(np_state_t* context, np_simple_cache_table_t* cache)
+{
+    for (uint16_t i = 0; i < cache->_bucket_size; i++) 
+    {
+        sll_iterator(np_cache_item_t) iter_bucket_item = sll_first(&cache->_bucket[i]);
+        while(iter_bucket_item != NULL) {
+            if(iter_bucket_item->val.key != NULL) 
+            {
+                free(iter_bucket_item->val.key);
+            }
+            sll_next(iter_bucket_item);
+        }
+        sll_clear(np_cache_item_t, &cache->_bucket[i]);
+        np_spinlock_destroy(cache->_bucket_guard[i]);
+    }
+
+    free(cache->_bucket);
+    free(cache->_bucket_guard);
+}
+
+bool 
+np_simple_cache_get(np_state_t* context, const np_simple_cache_table_t *table, const char* const key, void** value)
+{    
+    ASSERT(key!= NULL, "cache key cannot be NULL!");
+
+    log_trace_msg(LOG_TRACE, "start: np_cache_item_t* np_simple_cache_get(np_simple_cache_table_t *table, const char *key){");
+
+    bool ret = false;
+
+    unsigned char bucket_hash[crypto_shorthash_BYTES];
+    uint64_t bucket_bigint = 0;
+    crypto_shorthash_siphash24(bucket_hash,  (const unsigned char*) key, strnlen(key, 255), table->_seed);    
+    memcpy(&bucket_bigint, &bucket_hash[0], crypto_shorthash_BYTES);
+    uint16_t bucket = ((uint16_t)(bucket_bigint) & 0xffff) % (table->_bucket_size);
+
+    log_debug_msg(LOG_DEBUG, "cache::get() %d -> %s (%d)", bucket, key, sll_size(&table->_bucket[bucket]));
+    np_spinlock_lock(&table->_bucket_guard[bucket]);
+    {
+        sll_iterator(np_cache_item_t) iter = sll_first(&table->_bucket[bucket]);
+        while (NULL != iter )
+        {
+            if(NULL != iter->val.key && 
+               strncmp(iter->val.key, key, strnlen(iter->val.key, 255) ) == 0)
+            {
+                *value = iter->val.value;
+                ret = true;
+                break;
+            }
+            sll_next(iter);
+        }
+    }
+    np_spinlock_unlock(&table->_bucket_guard[bucket]);
+    return ret;
+}
+
+bool 
+np_simple_cache_add(np_state_t* context, const np_simple_cache_table_t *table, const char* const key, void *value)
+{
+    // Contract
+    ASSERT(key!= NULL, "cache key cannot be NULL!");
+    // Contract end
+
+    log_trace_msg(LOG_TRACE, "start: int np_simple_cache_insert(context, np_simple_cache_table_t *table, char *key, void *value) {");
+
+    bool ret = false;
+
+    unsigned char bucket_hash[crypto_shorthash_BYTES];
+    uint64_t bucket_bigint = 0;
+    crypto_shorthash_siphash24(bucket_hash, (const unsigned char*) key, strnlen(key, 255), table->_seed);    
+    memcpy(&bucket_bigint, &bucket_hash[0], crypto_shorthash_BYTES);
+    uint16_t bucket = ((uint16_t) (bucket_bigint) & 0xffff) % (table->_bucket_size);
+
+    log_debug_msg(LOG_DEBUG, "cache::add() %d -> %s (%d)", bucket, key, sll_size(&table->_bucket[bucket]));
+    np_spinlock_lock(&table->_bucket_guard[bucket]);
+    {
+        bool found = false;
+        sll_iterator(np_cache_item_t) iter = sll_first(&table->_bucket[bucket]);
+        while (NULL != iter)
+        {
+            if(NULL != iter->val.key && 
+               strncmp(iter->val.key, key, strnlen(iter->val.key, 255) ) == 0)
+            {
+                found = true;
+                break;
+            }
+            sll_next(iter);
+        }
+
+        if (!found) 
+        {
+            np_cache_item_t item = {
+                .insert_time = np_time_now(), 
+                .key=strndup(key, strnlen(key, 255)),
+                .value = value
+            };
+            sll_append(np_cache_item_t, &table->_bucket[bucket], item);
+            ret = true;
+        }
+    }
+    np_spinlock_unlock(&table->_bucket_guard[bucket]);
+    return ret;
 }
