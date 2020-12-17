b5495172d2a3da40397398e82564b8619367cce7
Simon Klampt <simon.klampt@pi-lar.net>
2020-12-17 11:10:24 +0100

    version bump

--- bindings/python_cffi/setup.py
+++ bindings/python_cffi/setup.py
@@ -15,7 +15,7 @@ if PATH:
     os.chdir(PATH)
 
 setup (name = 'neuropil',
-       version = '0.9.7',
+       version = '0.9.8',
        author      = "pi-lar GmbH",
        description = """neuropil python bindings""",
        setup_requires=["cffi>=1.0.0"],
