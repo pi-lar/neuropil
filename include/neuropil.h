ebba35f834e397a439d735d5f31c347d35743d8f
Simon Bonzelet <simon.klampt@pi-lar.net>
2021-04-13 15:37:50 +0200

    version bump

--- include/neuropil.h
+++ include/neuropil.h
@@ -18,7 +18,7 @@
 extern "C" {
 #endif
 
-#define NEUROPIL_RELEASE	"neuropil_0.9.8"
+#define NEUROPIL_RELEASE	"neuropil_0.9.9"
 
 #define NEUROPIL_COPYRIGHT	"copyright (C) 2016-2021 neuropil.org, Cologne, Germany"
 #define NEUROPIL_TRADEMARK  "trademark (TM) 2016-2021 pi-lar GmbH, Cologne, Germany"
