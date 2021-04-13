48d59e095d147706378486ccc09e769ce8313c7d
Simon Bonzelet <simon.klampt@pi-lar.net>
2021-04-13 15:36:05 +0200

    test different msg sizes

--- test/smoke/test_msg_delivery.py
+++ test/smoke/test_msg_delivery.py
@@ -9,15 +9,20 @@ from misc import TestHelper
 from multiprocessing import Value
 from ctypes import c_char, c_bool
 
-class MsgDeliveryTest(unittest.TestCase):
-    msg_delivery_succ = Value(c_bool, False)
+import random
+import string
+import sys
 
-    @staticmethod
-    def msg_received(node:NeuropilNode, message:np_message):
-        MsgDeliveryTest.msg_delivery_succ.value = True
+class MsgDeliveryTest(unittest.TestCase):
+
+    def msg_received(self, node:NeuropilNode, message:np_message):
+        self.msg_delivery_succ.value = True
+        self.assertEqual(sys.getsizeof(message.raw()), self.target_size)
         return True
 
-    def test_msg_delivery(self):
+    def _test_msg_X_delivery(self, size):
+        self.msg_delivery_succ = Value(c_bool, False)
+        self.target_size = size
 
         np_c = NeuropilCluster(    3, port_range=4010, auto_run=False, log_file_prefix="logs/smoke_test_msg_delivery_cl_")
         np_1 = NeuropilNode(4001, log_file="logs/smoke_test_msg_delivery_nl1.log", auto_run=False, no_threads=6)
@@ -44,22 +49,24 @@ class MsgDeliveryTest(unittest.TestCase):
         np_2.join(np1_addr)
         np_c.join(np2_addr)
 
-        timeout = 120 #sec
+        timeout = 180 #sec
 
         t1 = time.time()
         elapsed = 0.
         send = False
+        data = ''.join(random.choice(string.ascii_uppercase) for i in range(self.target_size-33)).encode("utf-8")
+
         try:
-            while elapsed < timeout and not MsgDeliveryTest.msg_delivery_succ.value:
+            while elapsed < timeout and not self.msg_delivery_succ.value:
                 elapsed = float(time.time() - t1)
 
                 if np_1.np_has_receiver_for(subject) and not send:
-                    if np_1.send(subject, b'test') != neuropil.np_ok:
+                    if np_1.send(subject, data) != neuropil.np_ok:
                         print("ERROR sending Data")
                     else:
                         send = True
 
-                if MsgDeliveryTest.msg_delivery_succ.value:
+                if self.msg_delivery_succ.value:
                     break
                 np_1.run(math.pi/10)
 
@@ -69,4 +76,15 @@ class MsgDeliveryTest(unittest.TestCase):
             np_c.shutdown()
 
         self.assertTrue(send)
-        self.assertTrue(MsgDeliveryTest.msg_delivery_succ.value)
+        self.assertTrue(self.msg_delivery_succ.value)
+
+    def test_msg_1k_delivery(self):
+        self._test_msg_X_delivery(1000)
+    def test_msg_10k_delivery(self):
+        self._test_msg_X_delivery(1000*50)
+    #def test_msg_100k_delivery(self):
+    #    self._test_msg_X_delivery(1000*100)
+    #def test_msg_1MB_delivery(self):
+    #    self._test_msg_X_delivery(1000*1000)
+    #def test_msg_10MB_delivery(self):
+    #    self._test_msg_X_delivery(1000*1000*10)
