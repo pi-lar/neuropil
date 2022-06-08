abb7cbda179152eb574b509ad3933ddfc2bf9f98
simon.klampt <simon.klampt@pi-lar.net>
2022-06-08 13:02:26 +0200

    statistics require threads to be initiated

--- src/neuropil.c
+++ src/neuropil.c
@@ -208,13 +208,12 @@ np_context* np_new_context(struct np_settings * settings_in)
         log_msg(LOG_ERROR, "neuropil_init: could not init crypto library");
         status = np_startup;
     }
-    else if (_np_statistics_init(context) == false)
-    {
-        log_msg(LOG_ERROR, "neuropil_init: could not init statistics");
-        status = np_startup;
-    }
     else if (_np_threads_init(context) == false) {
         log_msg(LOG_ERROR, "neuropil_init: could not init threading mutexes");
+        status = np_startup;
+    }
+    else if (_np_statistics_init(context) == false) {
+        log_msg(LOG_ERROR, "neuropil_init: could not init statistics");
         status = np_startup;
     }
     else if (_np_event_init(context) == false)
